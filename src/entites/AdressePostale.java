051a449a87bdffaf4f719542eb88dfb528a03015
arny-wilk <arnywilk@gmail.com>
2023-05-17 17:04:43 +0200

    Bootstrap TP2 Instances et methodes

--- src/entites/AdressePostale.java
+++ src/entites/AdressePostale.java
@@ -12,4 +12,46 @@ public class AdressePostale {
         this.zipcode = zipcode;
         this.ville = ville;
     }
+
+    public int getNumero_rue() {
+        return numero_rue;
+    }
+
+    public void setNumero_rue(int numero_rue) {
+        this.numero_rue = numero_rue;
+    }
+
+    public String getLibelle_rue() {
+        return libelle_rue;
+    }
+
+    public void setLibelle_rue(String libelle_rue) {
+        this.libelle_rue = libelle_rue;
+    }
+
+    public int getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    @Override
+    public String toString() {
+        return "AdressePostale{" +
+                "numero_rue=" + numero_rue +
+                ", libelle_rue='" + libelle_rue + '\'' +
+                ", zipcode=" + zipcode +
+                ", ville='" + ville + '\'' +
+                '}';
+    }
 }
--- src/entites/TestPersonne.java
+++ src/entites/TestPersonne.java
@@ -8,7 +8,9 @@ public class TestPersonne {
         AdressePostale addr4 = new AdressePostale(8, "Rue Jean Moulin", 34500, "Béziers");
         Personne personne1 = new Personne("tata", "papa", addr4);
         Personne personne2 = new Personne("titi", "pipi", addr3);
-        personne1.isAdherent();
-        System.out.println(personne1.getNom());
+        personne1.print();
+        addr3.setLibelle_rue("Rue des Tulipes");
+        personne1.setAdressePostale(addr3);
+        personne1.print();
     }
 }
